2ad83e899f4b615d2e76ff0468cd723bfa416be7
haim <haimor1123@gmail.com>
2023-04-19 21:08:28 +0300

    final version

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-from  ex1_main import transformYIQ2RGB,imReadAndConvert,imDisplay,transformRGB2YIQ
+from  ex1_main import hsitogramEqualize,transformYIQ2RGB,imReadAndConvert,imDisplay,transformRGB2YIQ
 
 
 # imDisplay('bac_con.png',1)
 
-image=imReadAndConvert('beach.jpg',0)
+image=imReadAndConvert('beach.jpg',1)
 yiq_img=transformYIQ2RGB(image)
+
+
+img = imReadAndConvert('beach.jpg',0)
+hsitogramEqualize(img)
